9d6658e424c493920960f9bc5d1c085888b72919
sindhug <sindhu.ghanta@gmail.com>
2020-08-07 13:25:23 -0700

    Update emergency_service.py

--- emergency_service.py
+++ emergency_service.py
@@ -2,6 +2,8 @@ import pandas as pd
 import requests
 import json
 
+# This REST API was created in AIClub Navigator
+# You can interact with it here as well: https://aiclub.world/projects/32e764d4-0b61-4c36-bce9-2cb85903cc33?tab=service
 def get_prediction(data):
   url = 'https://4srz4f0vc4.execute-api.us-east-1.amazonaws.com/Predict/cc496ecb-0fca-4e5d-9240-7c344ea4f1f9'
   r = requests.post(url, data=json.dumps(data))
