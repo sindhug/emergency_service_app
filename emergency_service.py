6c67988a1bff9336edefb137dd46cdb429af2470
sindhug <sindhu.ghanta@gmail.com>
2020-08-07 13:22:56 -0700

    Create emergency_service.py

---
+++ emergency_service.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import requests
+import json
+
+def get_prediction(data):
+  url = 'https://4srz4f0vc4.execute-api.us-east-1.amazonaws.com/Predict/cc496ecb-0fca-4e5d-9240-7c344ea4f1f9'
+  r = requests.post(url, data=json.dumps(data))
+  response = getattr(r,'_content').decode("utf-8")
+  #print(response)
+  return response
+
+prediction = json.loads(json.loads(get_prediction())['body'])['predicted_label']
+print('The AI predicted: ', prediction)
